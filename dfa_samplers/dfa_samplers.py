51634f0ce7690aa68e1fe562d2f08b1328096f3e
Beyazit Yalcinkaya <beyazit@wifi-10-42-205-157.wifi.berkeley.edu>
2025-02-25 14:03:37 -0800

    Add min_size

--- dfa_samplers/dfa_samplers.py
+++ dfa_samplers/dfa_samplers.py
@@ -6,10 +6,12 @@ from dfa_mutate import change_transition
 __all__ = ["DFASampler", "ReachSampler", "ReachAvoidSampler", "RADSampler"]
 
 class DFASampler:
-    def __init__(self, n_tokens: int = 10, max_size: int = 10):
+    def __init__(self, n_tokens: int = 10, min_size: int = 3, max_size: int = 10):
         assert n_tokens > 1 and max_size >= 2
         self.n_tokens = n_tokens
+        self.min_size = min_size
         self.max_size = max_size
+        assert self.min_size >= 2 and self.max_size >= self.min_size
 
     def sample(self) -> DFA:
         dfa = self._sample()
@@ -39,7 +41,7 @@ class ReachSampler(DFASampler):
 
     def _sample(self) -> DFA:
         tokens = list(range(self.n_tokens))
-        n = np.random.randint(2, self.max_size) + 1
+        n = np.random.randint(self.min_size - 1, self.max_size) + 1
         success = n - 1
         transitions = {
             success: (True, {t: success for t in range(self.n_tokens)})
@@ -62,9 +64,10 @@ class ReachAvoidSampler(DFASampler):
     def __init__(self, prob_stutter: float = 0.9, p: float | None = None, **kwargs):
         super().__init__(**kwargs)
         assert 0 <= prob_stutter <= 1
+        assert self.min_size >= 3
         self.prob_stutter = prob_stutter
         self.p = p
-        self.n_values = np.array(list(range(1, self.max_size - 2 + 1)))
+        self.n_values = np.array(list(range(self.min_size - 2, self.max_size - 2 + 1)))
         if self.p is not None:
             self.n_p = np.array([(self.p)**v for v in self.n_values])
             self.n_p = self.n_p / np.sum(self.n_p)
@@ -112,13 +115,21 @@ class RADSampler(DFASampler):
             return d._transition(s, c)
         return DFA(start=d.start, inputs=d.inputs, label=d._label, transition=transition)
 
-    def _sample(self) -> DFA:
+    def _sample_rad(self) -> DFA:
         candidate = self.reach_avoid_sampler.sample()
-        for _ in range(np.random.choice(self.max_mutations + 1)):
+        for i in range(np.random.choice(self.max_mutations + 1)):
             tmp = self._accepting_is_sink(change_transition(candidate))
             if tmp is None: continue
             tmp = tmp.minimize()
             if len(tmp.states()) == 1:
                 continue
             candidate = tmp
+        return candidate
+
+    def _sample(self) -> DFA:
+        candidate = self._sample_rad()
+        n_states = len(candidate.states())
+        while n_states < self.min_size or n_states > self.max_size:
+            candidate = self._sample_rad()
+            n_states = len(candidate.states())
         return candidate
--- test.py
+++ test.py
@@ -8,3 +8,16 @@ if __name__ == "__main__":
         assert rad_sampler.sample().find_word() is not None
         assert reach_sampler.sample().find_word() is not None
         assert reach_avoid_sampler.sample().find_word() is not None
+
+    a = 10
+    b = 20
+    rad_sampler = RADSampler(min_size=a, max_size=b)
+    reach_sampler = ReachSampler(min_size=a, max_size=b)
+    reach_avoid_sampler = ReachAvoidSampler(min_size=a, max_size=b)
+    for _ in range(100):
+        n_rad = len(rad_sampler.sample().states())
+        n_reach = len(reach_sampler.sample().states())
+        n_reach_avoid = len(reach_avoid_sampler.sample().states())
+        assert n_rad >= a and n_rad <= b
+        assert n_reach >= a and n_reach <= b
+        assert n_reach_avoid >= a and n_reach_avoid <= b
